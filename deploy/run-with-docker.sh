33682befe6ab369682985c763cbe53f0ec222951
Josh Long <josh@joshlong.com>
2020-10-22 04:07:10 -0700

    dep

--- deploy/run-with-docker.sh
+++ deploy/run-with-docker.sh
@@ -6,6 +6,6 @@ AN=$( docker images | grep rmq-app | cut -f1 -d\  )
 
 docker run \
   -e SPRING_RABBITMQ_HOST=host.docker.internal  \
-  -e SPRING_RABBITMQ_USERNAME=guest  \
-  -e SPRING_RABBITMQ_PASSWORD=guest  \
+  -e SPRING_RABBITMQ_USERNAME="bp-rmq"  \
+  -e SPRING_RABBITMQ_PASSWORD="bp-rmq"  \
   $( docker images -aq $AN )
