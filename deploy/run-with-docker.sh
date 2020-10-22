9afdf86475341d27fa8476d124bbb02a7f5369d4
Josh Long <josh@joshlong.com>
2020-10-22 02:25:57 -0700

    up

--- deploy/run-with-docker.sh
+++ deploy/run-with-docker.sh
@@ -1,13 +1,11 @@
 cd `dirname $0`/..
 
-
-
 mvn clean spring-boot:build-image
 
 AN=$( docker images | grep rmq-app | cut -f1 -d\  )
 
 docker run \
   -e SPRING_RABBITMQ_HOST=host.docker.internal  \
-  -e SPRING_RABBITMQ_USERNAME=bp  \
-  -e SPRING_RABBITMQ_PASSWORD=password  \
+  -e SPRING_RABBITMQ_USERNAME=guest  \
+  -e SPRING_RABBITMQ_PASSWORD=guest  \
   $( docker images -aq $AN )
